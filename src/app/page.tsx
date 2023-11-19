43393b3f9b4c5f5415659570b14947b21c174f93
Bilal Sattar <bilalsattar24@gmail.com>
2023-11-18 16:06:09 -0800

    Add table structure and sample data to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <div>Inventory Helper, the start of something potentially great</div>;
+  return (
+    <>
+      <table style={{ border: "1px solid black" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black" }}>product</th>
+            <th style={{ border: "1px solid black" }}>next order date</th>
+            <th style={{ border: "1px solid black" }}>
+              current inventory level
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid black" }}>product 1</td>
+            <td style={{ border: "1px solid black" }}>date 1</td>
+            <td style={{ border: "1px solid black" }}>inventory 1</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
