6d1a5c72d91b3ffb8393d7d3c15a29dc5f611083
Bilal Sattar <bilalsattar24@gmail.com>
2023-12-20 01:46:20 +0000

    use hard coded products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,38 @@
+"use client";
+import { useState } from "react";
+
+type Product = {
+  name: string;
+  nextOrderDate?: Date;
+  currentInventoryLevel: number;
+  sellRate: number;
+  leadTimeDays: number;
+};
+
+const defaultProducts = [
+  {
+    name: "product 1",
+    currentInventoryLevel: 1,
+    sellRate: 10,
+    leadTimeDays: 70,
+  },
+  {
+    name: "product 2",
+    currentInventoryLevel: 2,
+    sellRate: 2,
+    leadTimeDays: 70,
+  },
+  {
+    name: "product 3",
+    currentInventoryLevel: 3,
+    sellRate: 3,
+    leadTimeDays: 70,
+  },
+];
+
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>(defaultProducts);
+
   return (
     <>
       <h1>Inventory Assistant</h1>
@@ -7,19 +41,84 @@ export default function Home() {
           <tr>
             <th style={{ border: "1px solid black" }}>product</th>
             <th style={{ border: "1px solid black" }}>next order date</th>
+            <th style={{ border: "1px solid black" }}>sell rate</th>
             <th style={{ border: "1px solid black" }}>
               current inventory level
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td style={{ border: "1px solid black" }}>product 1</td>
-            <td style={{ border: "1px solid black" }}>date 1</td>
-            <td style={{ border: "1px solid black" }}>inventory 1</td>
-          </tr>
+          {products.map((product) => (
+            <tr>
+              <td style={{ border: "1px solid black" }}>{product.name}</td>
+              <td style={{ border: "1px solid black" }}>
+                {product.nextOrderDate?.toLocaleDateString()}
+              </td>
+              <td style={{ border: "1px solid black" }}>{product.sellRate}</td>
+
+              <td style={{ border: "1px solid black" }}>
+                {product.currentInventoryLevel}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
+      {/* ui for editing default products*/}
+      <h2>edit default products</h2>
+      {products.map((product, index) => {
+        return (
+          <div>
+            <label>name: </label>
+            <input
+              style={{ border: "1px solid black", marginRight: "10px" }}
+              type="number"
+              value={product.name}
+              onChange={(event) => {
+                const newProducts = [...products];
+                newProducts[index].name = event.target.value;
+                setProducts(newProducts);
+              }}
+            />
+            <label>current inventory level: </label>
+            <input
+              style={{ border: "1px solid black", marginRight: "10px" }}
+              type="number"
+              value={product.currentInventoryLevel}
+              onChange={(event) => {
+                const newProducts = [...products];
+                newProducts[index].currentInventoryLevel = Number(
+                  event.target.value
+                );
+                setProducts(newProducts);
+              }}
+            />
+            <label>sell rate: </label>
+            <input
+              style={{ border: "1px solid black", marginRight: "10px" }}
+              type="number"
+              value={product.sellRate}
+              onChange={(event) => {
+                const newProducts = [...products];
+                newProducts[index].sellRate = Number(event.target.value);
+                setProducts(newProducts);
+              }}
+            />
+            <label>lead time days: </label>
+            <input
+              style={{ border: "1px solid black", marginRight: "10px" }}
+              type="number"
+              value={product.leadTimeDays}
+              onChange={(event) => {
+                const newProducts = [...products];
+                newProducts[index].leadTimeDays = Number(event.target.value);
+                setProducts(newProducts);
+              }}
+            />
+          </div>
+        );
+        console.log(product);
+      })}
     </>
   );
 }
