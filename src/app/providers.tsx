e27ff97f6d18339eb6755587671cb9252da0fe98
Bilal Sattar <bilalsattar24@gmail.com>
2024-12-02 14:53:05 -0800

    chakra works

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
@@ -38,10 +37,8 @@ const theme = extendTheme({
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
       <ChakraProvider theme={theme}>
         {children}
       </ChakraProvider>
-    </CacheProvider>
   )
 }
